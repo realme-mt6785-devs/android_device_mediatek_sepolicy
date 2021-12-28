2765bf47ebeddf43076e8acb2e454f1647b81850
SamarV-121 <samarvispute121@pm.me>
2021-12-28 20:44:37 +0530

    sepolicy: Include neverallow rules
    Change-Id: I3c3d42339a0417dcd314b84ca4cf1632c5bbfef6
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,11 +1,14 @@
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS := \
+        device/mediatek/sepolicy/neverallows/non_plat \
         device/mediatek/sepolicy/non_plat
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
+        device/mediatek/sepolicy/neverallows/plat_public \
         device/mediatek/sepolicy/plat_public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
+        device/mediatek/sepolicy/neverallows/plat_private \
         device/mediatek/sepolicy/plat_private
 
 # Inherit lineage sepolicy for mediatek devices
