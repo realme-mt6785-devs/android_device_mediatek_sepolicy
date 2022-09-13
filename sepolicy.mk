9b9df632269e3b475976f8862b765f2516ad3904
SamarV-121 <samarvispute121@pm.me>
2022-09-13 10:44:36 +0530

    sepolicy: Drop debug policies
    Change-Id: I105c3f0b14ee1c062c4dc25d2b24ed382fb6f276

--- sepolicy.mk
+++ sepolicy.mk
@@ -3,19 +3,13 @@ MTK_SEPOLICY_PATH := device/mediatek/sepolicy
 
 BOARD_SEPOLICY_DIRS += \
     $(MTK_SEPOLICY_PATH)/basic/non_plat \
-    $(MTK_SEPOLICY_PATH)/basic/debug/non_plat \
     $(MTK_SEPOLICY_PATH)/bsp/non_plat \
-    $(MTK_SEPOLICY_PATH)/bsp/debug/non_plat \
     $(MTK_SEPOLICY_PATH)/modem
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     $(MTK_SEPOLICY_PATH)/basic/plat_private \
-    $(MTK_SEPOLICY_PATH)/basic/debug/plat_private \
     $(MTK_SEPOLICY_PATH)/bsp/plat_private \
-    $(MTK_SEPOLICY_PATH)/bsp/debug/plat_private
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     $(MTK_SEPOLICY_PATH)/basic/plat_public \
-    $(MTK_SEPOLICY_PATH)/basic/debug/plat_public \
     $(MTK_SEPOLICY_PATH)/bsp/plat_public \
-    $(MTK_SEPOLICY_PATH)/bsp/debug/plat_public
